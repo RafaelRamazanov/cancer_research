568bd5b8eea6c2828c629d70aab24f2444566517
Rafael Ramazanov <r.ramazanov@of-group.ru>
2025-10-12 21:34:25 +0300

    major update

--- app.py
+++ app.py
@@ -3,34 +3,33 @@ import pandas as pd
 import datetime
 from model import Classifier
 
+language = st.sidebar.selectbox('', ['ru', 'en'])
+
 st.image("logo.png", width=500)
-st.header('ИИ-система поддержки принятия решений при проведении биопсии предстательной железы')
-st.subheader('AI Decision Support for Prostate Biopsy')
 
-
-def get_input():
-    height = st.number_input("Рост, см (Height, cm)")
-    weight = st.number_input("Вес, кг (Weight, cm)")
-    date_of_birth = st.date_input("Дата рождения (Date of birth)",
+def get_input_ru():
+    height = st.number_input("Рост, см", min_value=0)
+    weight = st.number_input("Вес, кг", min_value=0)
+    date_of_birth = st.date_input("Дата рождения",
                                   min_value=datetime.date(1900, 1, 1))
-    relatives = st.number_input("""Наличие рака простаты у ближайших родственников 
-                                   (Positive family history of prostate cancer)""")
-    opsa = st.number_input("""оПСА (нг/мл)
-                          (Total PSA level)""")
-    phi = st.number_input("""Индекс здоровья простаты,
-                            Prostate health index""")
-    date = st.date_input("""Дата МРТ (Date of MRI)""",
+    relatives = st.radio("""Наличие рака простаты у ближайших родственников""",
+                        ["Нет", "Да"])
+    relatives = 1 if relatives == "Да" else 0
+
+    opsa = st.number_input("""оПСА (нг/мл)""")
+    phi = st.number_input("""Индекс здоровья простаты""", min_value=0)
+
+    date = st.date_input("""Дата МРТ""",
                          min_value=datetime.date(1900, 1, 1))
-    pi_rads = st.number_input("Данные МРТ, PI-RADS")
-    suspicious_areas = st.number_input("""Подозрительные очаги при УЗИ
-                                       (Suspicious lesions on TRUS)""")
-    volume_of_suspicious_areas = st.number_input("""Обьем подозрительных участков (см3)
-                                                    (Volume of suspicious prostate lesions on TRUS (cm3))""")
-    pri = st.number_input("""Данные ПРИ (укажите 1 - при наличии подозрительных изменений (уплотнение и др.),
-                             0 - при гомогенной железе без изменений) 
-                            (DRE findings:
-                            1 - suspicious changes (e.g., induration, nodularity)
-                            0 - normal (homogeneous prostate))""")
+    pi_rads = st.select_slider("Данные МРТ, PI-RADS", [1, 2, 3, 4, 5])
+    suspicious_areas = st.radio("""Подозрительные очаги при УЗИ""",
+                                ["Нет", "Да"])
+    suspicious_areas = 1 if suspicious_areas == "Да" else 0
+
+    volume_of_suspicious_areas = st.number_input("""Обьем подозрительных участков (см3)""")
+    pri = st.radio("""Данные ПРИ (укажите 1 - при наличии подозрительных изменений (уплотнение и др.),
+                             0 - при гомогенной железе без изменений)""",
+                          [0, 1])
 
     date_of_birth = (pd.to_datetime('today') - pd.to_datetime(date_of_birth)).days
     date = (pd.to_datetime('today') - pd.to_datetime(date)).days
@@ -49,29 +48,104 @@ def get_input():
 
     return features
 
-features = get_input()
-with open('warning.txt', 'r', encoding="utf8") as f:
-    warning = f.read()
+def get_input_en():
+    height = st.number_input("Height, cm", min_value=0)
+    weight = st.number_input("Weight, cm", min_value=0)
+    date_of_birth = st.date_input("Date of birth",
+                                  min_value=datetime.date(1900, 1, 1))
+    relatives = st.radio("""Positive family history of prostate cancer""",
+                        ["No", "Yes"])
+    relatives = 1 if relatives == "Yes" else 0
 
-st.warning(warning)
+    opsa = st.number_input("""Total PSA level""")
+    phi = st.number_input("""Prostate health index""", min_value=0)
 
-if st.button('Получить результат'):
-    model = Classifier()
-    prediction = model.predict(features)[1]
+    date = st.date_input("""Date of MRI""",
+                         min_value=datetime.date(1900, 1, 1))
+    pi_rads = st.select_slider("PI-RADS", [1, 2, 3, 4, 5])
+    suspicious_areas = st.radio("""Suspicious lesions on TRUS""",
+                                ["No", "Yes"])
+    suspicious_areas = 1 if suspicious_areas == "Yes" else 0
 
-    st.text("Вероятность наличия рака предстательной железы: {:.2f}%".format(prediction))
+    volume_of_suspicious_areas = st.number_input("""Volume of suspicious prostate lesions on TRUS (cm3)""")
+    pri = st.radio("""DRE findings:
+                      1 - suspicious changes (e.g., induration, nodularity)
+                      0 - normal (homogeneous prostate))""",
+                    [0, 1])
 
-    if prediction >= 0.5:
-        st.error("Рекомендуется выполнение трансректальной биопсии предстательной железы (Transrectal prostate biopsy is recommended)")
-    elif prediction >= 0.45:
-        st.warning("Рекомендумется выполнение трансперинеальной фьюжн-биопсии предстательной железы (Fusion-guided transperineal prostate biopsy is recommended)")
-    else:
-        st.success("Рекомендуется динамическое наблюдение (Active surveillance is recommended)")
+    date_of_birth = (pd.to_datetime('today') - pd.to_datetime(date_of_birth)).days
+    date = (pd.to_datetime('today') - pd.to_datetime(date)).days
 
-st.divider()
+    features = [date_of_birth,
+                height,
+                weight,
+                relatives,
+                opsa,
+                phi,
+                date,
+                pi_rads,
+                pri,
+                suspicious_areas,
+                volume_of_suspicious_areas]
 
-st.caption("""Разработчики: коллектив врачей Московского урологического центра ММНКЦ им. С.П.Боткина""")
-st.caption("""Developed by: Medical team of the Moscow Urology Center, Botkin Hospital""")
+    return features
 
-st.caption("""Контакт для связи: dr.p.arutyunyan@gmail.com""")
-st.caption("Contact: dr.p.arutyunyan@gmail.com")
+if language == 'ru':
+    st.header('ИИ-система поддержки принятия решений при проведении биопсии предстательной железы')
+    features = get_input_ru()
+    with open('warning_ru.txt', 'r', encoding="utf8") as f:
+        warning = f.read()
+
+    with st.expander("Информация"):
+        st.warning(warning)
+
+    if st.button('Получить результат'):
+        model = Classifier()
+        prediction = int(model.predict([features])[0][1] * 100)
+
+        st.text("Вероятность наличия рака предстательной железы: {:.2f}%".format(prediction))
+
+        if prediction >= 80:
+            st.error(
+                "Рекомендуется выполнение трансректальной биопсии предстательной железы (Transrectal prostate biopsy is recommended)")
+        elif prediction >= 40:
+            st.warning(
+                "Рекомендумется выполнение трансперинеальной фьюжн-биопсии предстательной железы (Fusion-guided transperineal prostate biopsy is recommended)")
+        else:
+            st.success("Рекомендуется динамическое наблюдение (Active surveillance is recommended)")
+
+    st.divider()
+
+    st.caption("""Разработчики: коллектив врачей Московского урологического центра ММНКЦ им. С.П.Боткина""")
+    st.caption("""Контакт для связи: dr.p.arutyunyan@gmail.com""")
+
+else:
+    st.header('AI Decision Support for Prostate Biopsy')
+    features = get_input_en()
+    with open('warning_en.txt', 'r', encoding="utf8") as f:
+        warning = f.read()
+
+    with st.expander("Information"):
+        st.warning(warning)
+
+    if st.button('Get result'):
+        model = Classifier()
+        prediction = int(model.predict([features])[0][1] * 100)
+
+        st.text("Probability of prostate cancer presence: {:.2f}%".format(prediction))
+
+        if prediction >= 80:
+            st.error(
+                "Transrectal prostate biopsy is recommended")
+        elif prediction >= 40:
+            st.warning(
+                "Fusion-guided transperineal prostate biopsy is recommended")
+        else:
+            st.success("Active surveillance is recommended")
+
+    st.divider()
+
+    st.caption("""Developed by: Medical team of the Moscow Urology Center, Botkin Hospital""")
+    st.caption("Contact: dr.p.arutyunyan@gmail.com")
+
+
--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import shap
 
 class Classifier:
     def __init__(self):
-        self.model = joblib.load('model_cancer.pkl')
+        self.model = joblib.load('model_cancer_calibrated.pkl')
 
     def predict(self, data):
         return self.model.predict_proba(data)
@@ -12,3 +12,10 @@ class Classifier:
         explainer = shap.TreeExplainer(self.model)
         shap_values = explainer.shap_values(data)
         return shap.plots.force(shap_values[0, ...])
+
+
+
+    # Убрать сотые везде кроме опса и объема подозрительных участков
+    # сделать галочку у наличия рака простаты,данные при
+    # линейка в pi-rads
+    # калибровка модели
